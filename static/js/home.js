06c6cf3757c290af9070ce6bf61a5448f8f5a423
tanidashoya <t120575st@gmail.com>
2025-06-24 22:25:31 +0900

    タイトル修正

--- static/js/home.js
+++ static/js/home.js
@@ -14,51 +14,50 @@
 
 
 window.addEventListener('DOMContentLoaded', () => {
-  
-  // const title = document.querySelector('.title');
-  // const reframe = document.querySelector('.reframe');
-  // const empower = document.querySelector('.empower');
-  // const titleimg = document.querySelector('.title-img')
   const header = document.querySelector('header')
+  const title = document.querySelector('.title span')
+  const titleImg = document.querySelector('.title-img')
   const main = document.querySelector('main')
-
+  
     // 順番に時間差でクラス追加
     // setTimeoutは【第二引数に渡された時間が経過したのち、第一引数ののコードが実行される】
     // classList.add ⇒ 引数に渡したコードが追加される
     if (!sessionStorage.getItem('animationPlayed')){
-      // setTimeout(() => {
-      //   title.classList.add('show');
-      // }, 800); // すぐ
+      
+      setTimeout(() => {
+        const spans = document.querySelectorAll('.title span');
 
-      // setTimeout(() => {
-      //   reframe.classList.add('show');
-      // }, 2400); // 1.2秒後（connectが表示されてから）
+        spans.forEach(span => {
+          span.style.animation = 'none';
+          span.style.opacity = getComputedStyle(span).opacity;
+        });
 
-      // setTimeout(() => {
-      //   empower.classList.add('show');
-      // }, 4000); // さらに1.2秒後（reframeのあと）
+        requestAnimationFrame(() => {
+          spans.forEach(span => {
+            span.style.opacity = '0';
+          });
+        });
+      }, 4000);
 
-      // setTimeout(() => {
-      //   titleimg.classList.add('show');
-      // }, 3600);
+      setTimeout(() => {
+        titleImg.classList.add('show')
+      },4000)
 
       setTimeout(() => {
         header.classList.add('show')
-      },6000)
+      },7000)
 
       setTimeout(() => {
         main.classList.add('show')
-      },6000)
+      },7000)
 
       sessionStorage.setItem('animationPlayed','true')
 
       // 2回目以降
     } else {
-      // title.classList.add('show');
-      // reframe.classList.add('show');
-      // empower.classList.add('show');
-      // titleimg.classList.add('show');
       header.classList.add('show');
+      title.classList.add('cover')
+      titleImg.classList.add('show');
       main.classList.add('show');
     }
 });
